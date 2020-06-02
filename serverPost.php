c9db97f3adafd8ae1969b710fc2045e09595e8db
Glen <me@glenwatson.me>
2020-06-02 04:03:21 -0700

    Added peristent client pairings to the signaling server scripts

--- serverPost.php
+++ serverPost.php
@@ -15,10 +15,7 @@ if (strlen($unique)==0 || ctype_digit($unique)===false) {
     
 // Add the new message to file
 $filename = '_file_' /*.$room*/ . $unique;
-if (file_exists($filename) && filesize($filename) != 0) {
-    file_put_contents($filename, '_MULTIPLEVENTS_', FILE_APPEND | LOCK_EX);
-}
 $posted = file_get_contents('php://input');
-file_put_contents($filename, $posted, FILE_APPEND | LOCK_EX);
+file_put_contents($filename, $posted.PHP_EOL, FILE_APPEND | LOCK_EX);
 
 ?>
