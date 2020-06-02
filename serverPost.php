8a1e5252e2c9b622265102a13c0b3b800cbe4d13
Glen <me@glenwatson.me>
2020-06-01 22:30:33 -0700

    Got signalling to work
    Now I need a STUN server

---
+++ serverPost.php
@@ -0,0 +1,37 @@
+<?php
+// SEE https://github.com/nielsbaloe/webrtc-php
+
+//CORS
+header('Access-Control-Allow-Origin: *', false);
+// A unique identifier (not necessary when working with websockets)
+if (!isset($_GET['unique'])) {
+    die('no identifier');
+}
+$unique=$_GET['unique'];
+if (strlen($unique)==0 || ctype_digit($unique)===false) {
+    die('not a correct identifier');
+}
+
+    
+// A main lock to ensure save safe writing/reading
+$mainlock = fopen('serverGet.php','r');
+if ($mainlock===false) {
+    die('could not create main lock');
+}
+flock($mainlock, LOCK_EX);
+   
+// Add the new message to file
+$filename = '_file_' /*.$room*/ . $unique;
+$file = fopen($filename,'ab');
+if (filesize($filename)!=0) {
+    fwrite($file,'_MULTIPLEVENTS_');
+}
+$posted = file_get_contents('php://input');
+fwrite($file,$posted);
+fclose($file);
+
+// Unlock main lock
+flock($mainlock,LOCK_UN);
+fclose($mainlock);
+
+?>
